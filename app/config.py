c4cab0385bf5291e96fb3fedd72188a54528de95
Nguyen Quang Tri <tri09t4@gmail.com>
2025-01-25 18:24:53 +0700

    add comments

--- app/config.py
+++ app/config.py
@@ -8,5 +8,6 @@ load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)),
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
 
+# Throw error in case no SUPABASE_URL or SUPABASE_API_KEY found
 if not SUPABASE_URL or not SUPABASE_API_KEY:
     raise ValueError("Supabase URL or API key is missing in environment variables")
